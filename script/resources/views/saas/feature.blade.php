c6eba978478ad7e75cae87ed0f30716cdf1008d4
rokan <hurokan91@gmail.com>
2021-07-27 18:57:26 +0600

    feature

--- script/resources/views/saas/feature.blade.php
+++ script/resources/views/saas/feature.blade.php
@@ -483,6 +483,12 @@
 
                     <div id="hs_cos_wrapper_widget_1596719451716" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_module" style="" data-hs-cos-general-type="widget" data-hs-cos-type="module">
                         <section class="logo-section">
+                             <div class="row small-row">
+                                <div class="column large-12">
+                                    <h2>{{$trFrontDetail->client_title}}</h2>
+                                    <p>{{$trFrontDetail->client_detail}}</p>
+                                </div>
+                            </div>
                             <div class="row small-up-2 medium-up-3 large-up-6" id="logo-row">
                                 @foreach($frontClients as $client)
                                     @php
